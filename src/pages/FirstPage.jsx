39b1c6d33d4fd304ba73a9882a17378b8d7f35f6
zhaotq <306403031@qq.com>
2019-09-17 17:18:26 +0800

    Initial commit

---
+++ src/pages/FirstPage.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button } from 'react-weui'
+
+
+//import styles
+import 'weui';
+import 'react-weui/build/packages/react-weui.css';
+
+class FirstPage extends Component{
+
+
+  render(){
+    return (
+      <div>
+        <Button>第一个按钮</Button>
+      </div>
+    )
+  }
+}
+export default FirstPage ;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+
+import Link from 'umi/link';
+import React from 'react';
+
+
+export default function() {
+
+  return (
+    <div>
+      <Link to={"/FirstPage"}>跳转实验</Link>
+    </div>
+  );
+}
